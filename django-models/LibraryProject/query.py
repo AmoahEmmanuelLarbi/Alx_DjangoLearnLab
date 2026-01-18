a181d93733f9e82777b7bb22911b44e185e8d9f0
Manuel <emmanuellarbiamoah@gmail.com>
2026-01-18 22:32:37 +0000

    Added a script for query

--- django-models/LibraryProject/query.py
+++ django-models/LibraryProject/query.py
@@ -2,4 +2,11 @@
 
 Ctrl click to launch VS Code Native REPL
 >>> <QuerySet [{'id': 1, 'title': 'Introduction to DSA', 'author_id': 1}, {'id': 2, 'title': 'Python for beginners', 'author_id': 1}]>
+>>> 16 objects imported automatically (use -v 2 for details).
+
+Ctrl click to launch VS Code Native REPL
+>>> <QuerySet [<Book: Introduction to DSA by James Smith>, <Book: Python for beginners by James Smith>, <Book: System Design by James Smith>]>
+>>> >>> <Book: Introduction to DSA by James Smith>
+>>> >>> <QuerySet [<Book: Introduction to DSA by James Smith>, <Book: Python for beginners by James Smith>, <Book: System Design by James Smith>]>
+>>> <QuerySet [{'id': 1, 'title': 'Introduction to DSA', 'author_id': 1}, {'id': 2, 'title': 'Python for beginners', 'author_id': 1}, {'id': 3, 'title': 'System Design', 'author_id': 1}]>
 >>> 
--- django-models/LibraryProject/relationship_app/query_sample.py
+++ django-models/LibraryProject/relationship_app/query_sample.py
@@ -1,7 +1,11 @@
 from django.db import models
-from .models import Author, Book, Library
+from relationship_app.models import Author, Book, Library
 
 Book.objects.all()
+"""
+<QuerySet [<Book: Introduction to DSA by James Smith>, <Book: Python for beginners by James Smith>, <Book: System Design by James Smith>]>
+"""
+
 
 author = Author.objects.create(name="Jones Acer")
 book1 = Book.objects.create(title="System Design", author=author)
@@ -11,3 +15,41 @@ library = Library.objects.create(name="Alx Library")
 library.books.add(book1)
 
 Library.objects.all()
+
+def query_all_books_by_author():
+    """
+    Docstring for query_all_books_by_author
+    Query all books by a specific autor
+    """
+    return Book.objects.filter(author_id = 1)
+
+from relationship_app.models import Book, Library, Librarian
+
+
+# Query 1: Query all books by a specific author
+def get_books_by_author(author_name):
+    """
+    Returns all books written by a specific author.
+    """
+    return Book.objects.filter(author__iexact=author_name)
+
+
+# Query 2: List all books in a library
+def get_books_in_library(library_name):
+    """
+    Returns all books available in a specific library.
+    """
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
+
+
+# Query 3: Retrieve the librarian for a library
+def get_librarian_for_library(library_name):
+    """
+    Returns the librarian responsible for a specific library.
+    """
+    library = Library.objects.get(name=library_name)
+    return library.librarian
+
+
+query_all_books_by_author()
