604e6232a8e1d5b774d81bef17ea5d27f0b9dad6
Manuel <emmanuellarbiamoah@gmail.com>
2026-01-20 03:51:02 +0000

    Retrieve data from library

--- django-models/LibraryProject/relationship_app/query_sample.py
+++ django-models/LibraryProject/relationship_app/query_sample.py
@@ -62,3 +62,28 @@ def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
     return library.librarian
 
+from relationship_app.models import Author, Book, Library
+
+
+def query_books_by_author(author_name):
+    """
+    Query all books by a specific author.
+    """
+    author = Author.objects.get(name=author_name)
+    return Book.objects.filter(author=author)
+
+
+def list_books_in_library(library_name):
+    """
+    List all books in a library.
+    """
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
+
+
+def get_library_librarian(library_name):
+    """
+    Retrieve the librarian for a library.
+    """
+    library = Library.objects.get(name=library_name)
+    return library.librarian
