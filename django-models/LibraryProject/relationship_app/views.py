ced44e6e0a3918c3af9225b64c2add7e6284c3e8
Manuel <emmanuellarbiamoah@gmail.com>
2026-01-20 22:37:52 +0000

    Implement a Function-based view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,4 +6,4 @@ from .models import Book
 def list_all_books(request):
     books = Book.objects.all() #retrieve all books
     context = {'books': books}
-    return render(request, 'list_books.html', context)
+    return render(request, 'relationship_app/list_books.html', context)
