02d310c96b3d5651db26303a327b0ee6fb16d589
Manuel <emmanuellarbiamoah@gmail.com>
2026-01-20 20:13:22 +0000

    Added a script to implement queries

--- django-models/LibraryProject/relationship_app/query_sample.py
+++ django-models/LibraryProject/relationship_app/query_sample.py
@@ -2,3 +2,6 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 
 Book.objects.all().values()
+author_name = "James Smith"
+Author.objects.get(name= author_name)
+# Author.objects.filter(author = author)
