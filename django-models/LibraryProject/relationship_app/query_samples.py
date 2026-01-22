bfdb430cc1b91bb9a862052bbf3b2547f50343f4
Manuel <emmanuellarbiamoah@gmail.com>
2026-01-22 16:50:05 +0000

    Added a script to query data

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,55 +1,16 @@
-from django.db import models
-from relationship_app.models import Author, Book, Library
-
-Book.objects.all()
-"""
-<QuerySet [<Book: Introduction to DSA by James Smith>, <Book: Python for beginners by James Smith>, <Book: System Design by James Smith>]>
-"""
+from relationship_app.models import Author, Book, Library, Librarian
 
 
-author = Author.objects.create(name="Jones Acer")
-book1 = Book.objects.create(title="System Design", author=author)
-Book.objects.filter(author_id=1)
+Book.objects.all().values()
+author = Author.objects.create(name = "Kim Joe")
+author_name = "James Smith"
 
-library = Library.objects.create(name="Alx Library")
-library.books.add(book1)
+Author.objects.get(name=author_name)
+Author.objects.filter(author=author)
+Book.objects.filter(author=author)
+Author.objects.get(name=author_name)
 
-Library.objects.all()
-
-def query_all_books_by_author():
-    """
-    Docstring for query_all_books_by_author
-    Query all books by a specific autor
-    """
-    return Book.objects.filter(author_id = 1)
-
-from relationship_app.models import Book, Library, Librarian
+Librarian.objects.all().values()
 
 
-# Query 1: Query all books by a specific author
-def get_books_by_author(author_name):
-    """
-    Returns all books written by a specific author.
-    """
-    return Book.objects.filter(author__iexact=author_name)
-
-
-# Query 2: List all books in a library
-def get_books_in_library(library_name):
-    """
-    Returns all books available in a specific library.
-    """
-    library = Library.objects.get(name=library_name)
-    return library.books.all()
-
-
-# Query 3: Retrieve the librarian for a library
-def get_librarian_for_library(library_name):
-    """
-    Returns the librarian responsible for a specific library.
-    """
-    library = Library.objects.get(name=library_name)
-    return library.librarian
-
-
-query_all_books_by_author()
+# ["Author.objects.get(name=author_name)", "objects.filter(author=author)"]
