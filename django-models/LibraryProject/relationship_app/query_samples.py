4fcda73aef92ca01bf0cb8480f0f1431b461f719
Manuel <emmanuellarbiamoah@gmail.com>
2026-01-22 17:11:52 +0000

    Added a script to query data

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,10 +1,15 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
+library_name = "Alx Library"
+# list all books in the library
+Book.objects.get(name=library_name)
+Book.objects.all()
 
 Book.objects.all().values()
 author = Author.objects.create(name = "Kim Joe")
 author_name = "James Smith"
 
+# query all books by a specific author
 Author.objects.get(name=author_name)
 Author.objects.filter(author=author)
 Book.objects.filter(author=author)
