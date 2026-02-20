16792eeb78b05ec9ba1c71577e0f9cfa475f0516
Manuel <emmanuellarbiamoah@gmail.com>
2026-02-20 00:06:08 +0000

    implementing custom user model

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,43 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User, AbstractUser, BaseUserManager
 
-"""
-Custom User Model
-"""
-
-
-# custom user manager
-class CustomManager(BaseUserManager):
-    # create a regular user
-    def create_user(self, email, username, password, **extra_fields):
-        if not email:
-            raise ValueError("Email is required")
-        email = self.normalize_email(email)
-        user = self.model(username=username, email=email)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    # create super user
-    def create_superuser(self, email, username, password, **extra_fields):
-        extra_fields.setdefault("is_active", True)
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-
-        return self.create_user(email, username, password, **extra_fields)
-
-
-# custom user model
-class CustomUser(AbstractUser):
-    # new fields to add
-    date_of_birth = models.DateField(null=False, verbose_name="date of birth")
-    profile_photo = models.ImageField(
-        upload_to="profile_photos/", blank=True, null=True
-    )
-
-    # use custom manager
-    objects = CustomManager()
-
 
 # Create your models here.
 # Author Model
