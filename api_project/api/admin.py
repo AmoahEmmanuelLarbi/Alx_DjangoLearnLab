ff6277bf6ae49a9e63498b10f9c27b6fab7f726b
Manuel <emmanuellarbiamoah@gmail.com>
2026-03-08 23:34:02 +0000

    implementing authentication and permissions in DRF

--- api_project/api/admin.py
+++ api_project/api/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Book
+
 
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    model = Book
+    list_display = [
+        "title",
+        "author",
+    ]
+    ordering = ("author",)
+
+
+admin.site.register(Book, BookAdmin)
--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd-party
     "rest_framework",
+    "rest_framework.authtoken",
+    # local
     "api.apps.ApiConfig",
 ]
 
@@ -117,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
