85ec99f5a6c701b44b62079db1d6f511f9918905
Manuel <emmanuellarbiamoah@gmail.com>
2026-03-13 06:22:31 +0000

    building custom views and generic views in django-rest framework

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,7 +5,7 @@ from .views import ListView, DetailView, CreateView, DeleteView, UpdateView
 urlpatterns = [
     path("books/", ListView.as_view(), name="books"),
     path("book/<int:pk>/", DetailView.as_view(), name="book-detail"),
-    path("book/update/<int:pk>/", UpdateView.as_view(), name="book-update"),
-    path("book/create", CreateView.as_view(), name="create-book"),
-    path("book/delete/<int:pk>/", DeleteView.as_view()),
+    path("books/update/<int:pk>/", UpdateView.as_view(), name="book-update"),
+    path("books/create/", CreateView.as_view(), name="create-book"),
+    path("books/delete/<int:pk>/", DeleteView.as_view()),
 ]
