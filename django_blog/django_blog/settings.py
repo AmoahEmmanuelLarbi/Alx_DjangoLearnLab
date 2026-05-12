ca4756f014d714cd497e76d002534471d810eddf
Manuel <emmanuellarbiamoah@gmail.com>
2026-05-12 14:41:40 +0000

    fix: update views

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -160,7 +160,8 @@ USE_TZ = True
 
 # new configuration for static files
 STATIC_URL = "/static/"
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # STATIC_URL = "static/"
