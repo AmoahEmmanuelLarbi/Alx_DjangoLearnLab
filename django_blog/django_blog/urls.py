4a358b91dacdaf2227b23b288dcf9e34c0bf66d1
Manuel <emmanuellarbiamoah@gmail.com>
2026-05-12 22:46:19 +0000

    fix: modify views

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("dashboard-admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("blog/", include("blog.urls")),
 ]
